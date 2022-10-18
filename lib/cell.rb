9e8ee449a85747ab86d564ffd35a2e7b8cfcd860
Brandon Gray <brandonjoe88@hotmail.com>
2022-10-17 19:17:50 -0600

    feat: Add coordinate, ship, and empty? methods with appropriate test stubs

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,5 +1,21 @@
+# require './lib/ship'
+
 class Cell
-  def initialize()
-    
+  def initialize(value)
+    @value = value
+    @cell = {@value => nil}
+    @ship = nil
+  end
+
+  def coordinate
+    @value
+  end
+
+  def ship
+    @ship
+  end
+
+  def empty?
+    @cell[@value].nil?
   end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -1,12 +1,37 @@
+# require './lib/ship'
 require './lib/cell'
 require 'rspec'
 
 describe Cell do
   describe '#initialize?' do
     it 'Creates instance of Cell' do
-      cell = Cell.new()
+      cell = Cell.new("B4")
 
       expect(cell).to be_instance_of(Cell)
+    end
+
+    it 'Cell is empty when created' do
+      cell = Cell.new("B4")
+
+      expect(cell.coordinate).to eq("B4")
+      expect(cell.ship).to eq(nil)
+      expect(cell.empty?).to be true
+    end
+  end
+
+  describe '#Ship placement' do
+    xit 'Accepts ship instance and places it' do
+      cell = Cell.new("B4")
+
+      expect
+    end
+  end
+
+  describe '#Ship damage when fired upon' do
+    xit 'Details effect of damage to ship' do
+      cell = Cell.new("B4")
+
+      expect
     end
   end
 end
